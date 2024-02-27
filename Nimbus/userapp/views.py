74de8c3d92c67fcef692e59f84069efe2b118d4a
KVK Siddartha <sidduultralegend@gmail.com>
2024-02-27 15:42:28 +0530

    completed the landing page

--- Nimbus/userapp/views.py
+++ Nimbus/userapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    count= 5
-    context = {'count': count}
-    return render(request, 'User/Landing.html', context)
+    return render(request, 'Non-User/Landing.html')
+
+def home(request):
+    return render(request, 'User/Home.html')
