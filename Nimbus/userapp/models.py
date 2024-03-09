98d5889db40811c66fc7793bb918eb56c339059f
KVK Siddartha <sidduultralegend@gmail.com>
2024-03-10 02:16:26 +0530

    fixed the search section along with the forms

--- Nimbus/userapp/models.py
+++ Nimbus/userapp/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 class City(models.Model):
-    cityID= models.AutoField(primary_key=True, blank=False)
-    name = models.CharField(max_length=25)
+    name = models.CharField(max_length=25, unique=True)
+    temperature = models.FloatField(null=True, blank=True)
+    humidity = models.FloatField(null=True, blank=True)
+    description = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
     class Meta:
         db_table = 'cities'
 
 class Users(models.Model):
     user= models.ForeignKey(User, on_delete=models.CASCADE)
+    mobile= models.CharField(max_length=10)
+    img= models.ImageField(upload_to="")
+    gender= models.CharField(max_length=10)
     
     class Meta:
         db_table= "Users"
--- Nimbus/userapp/views.py
+++ Nimbus/userapp/views.py
@@ -1,4 +1,5 @@
 from pyexpat.errors import messages
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 import requests
 from .forms import CityForm
@@ -30,23 +31,50 @@ def map(request):
     return render(request,'User/map.html')
 
 def search(request):
+    weather_data = City.objects.all()
+
     if request.method == 'POST':
-        place = request.POST['place']
-        API_KEY = '63407539ca53a7ee84abeced0dabbbb9'
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={place}&appid={API_KEY}'
-        response = requests.get(url)
+        place = request.POST.get('city', '')
+
+        if place:
+            API_KEY = '63407539ca53a7ee84abeced0dabbbb9'
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={place}&appid={API_KEY}'
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                data = response.json()
+                temperature = data['main']['temp']
+                temperature= temperature-273.15
+                temperature= '{:.1f}'.format(temperature)    
+                humidity = data['main']['humidity']
+                description = data['weather'][0]['description']
+
+                city, created = City.objects.get_or_create(
+                    name=place,
+                    defaults={'temperature': temperature, 'humidity': humidity, 'description': description}
+                )
+
+                return render(request, 'User/search.html', {'city': city, 'weather_data': weather_data})
+            else:
+                error_message = 'City not found. Please try again.'
+                return render(request, 'User/search.html', {'error_message': error_message, 'weather_data': weather_data})
+            
+    for i in weather_data:
+        response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={i.name}&appid=63407539ca53a7ee84abeced0dabbbb9')
         if response.status_code == 200:
-            data = response.json()
-            print(data)
-            temperature = data['main']['temp']
-            humidity = data['main']['humidity']
-            temperature1 = round(temperature - 273.15, 2)
-            return render(request, 'User/search.html',
-                          {'city': str.upper(place), 'temperature1': temperature1, 'humidity': humidity})
-        else:
-            error_message = 'City not found. Please try again.'
-            return render(request, 'User/search.html', {'error_message': error_message})
-    return render(request,"User/search.html")
+                data = response.json()
+                temperature = data['main']['temp']
+                temperature= temperature-273.15
+                temperature= '{:.1f}'.format(temperature)    
+                humidity = data['main']['humidity']
+                description = data['weather'][0]['description']
+
+                city = City.objects.get(name= i.name)
+                city.temperature= temperature
+                city.humidity= humidity
+                city.description= description
+                city.save()
+    return render(request, 'User/search.html', {'weather_data': weather_data})
 
 
 # def search(request):
