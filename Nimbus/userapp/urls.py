9f0d233110baafc5120cb131a9b404cf3cb45e6b
Arunsai57 <patriarunsai@gmail.com>
2024-03-09 17:19:46 +0530

    somehgin

--- Nimbus/userapp/urls.py
+++ Nimbus/userapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
-     path('guide', views.guide, name='guide'),
+    path('guide', views.guide, name='guide'),
+    path('map', views.map, name='map'),
+    path('search', views.search, name='search'),
+      
+     
 ]
